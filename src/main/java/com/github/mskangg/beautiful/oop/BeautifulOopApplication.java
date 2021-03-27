7beaa0b6c08ccc730a2f331ac50b60ce658201b4
mskangg <funnycoding91@gmail.com>
2021-03-27 17:37:06 +0900

    feat: project init

---
+++ src/main/java/com/github/mskangg/beautiful/oop/BeautifulOopApplication.java
@@ -0,0 +1,13 @@
+package com.github.mskangg.beautiful.oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulOopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautifulOopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mskangg/beautiful/oop/BeautifulOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mskangg.beautiful.oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
